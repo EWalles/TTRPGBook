0b809cdbf9cae004d935dd3d4fcf1342d13774cf
EWalles <ejomcdonald@gmail.com>
2022-09-20 16:28:53 -0400

    update

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-app.listen(process.env.PORT|3000, () => {
+app.listen(process.env.PORT||3000, () => {
     console.log('listening....');
 });
